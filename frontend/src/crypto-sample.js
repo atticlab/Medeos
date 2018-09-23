0bcf62bf5431a8fa2e7d42df6f7caba07fd9e6f8
Stanislav Cherviakov <stchervyakov@gmail.com>
2018-09-23 08:37:22 +0100

    get record sample

--- frontend/src/crypto-sample.js
+++ frontend/src/crypto-sample.js
@@ -7,6 +7,7 @@
    Eos = require('eosjs')
 
    const eoscob = require("@cobo/eos");
+   // Base58 = require("base-58");
 
  /** Transactions are only valid on the selected chain. */
   chain = {
@@ -33,48 +34,109 @@
     authorization: 'medeosdocto1@active',
   });
 
+console.log("keyProvider:", eosDoc.keyProvider)
+// eosDoc.contract('medeos111111').then(medeos111111 => medeos111111.regdoctor({"account":"medeosdocto1","name":"A good doctor", "speciality": "Family doctor", "hospital":"The Royal London Hospital"}).then(res => console.log("result", result))).catch(err=>console.log)
 
-eosDoc.contract('medeos111111').then(medeos111111 => medeos111111.regdoctor({"account":"medeosdocto1","name":"A good doctor", "speciality": "Family doctor", "hospital":"The Royal London Hospital"}).then(res => console.log("result", result))).catch(err=>console.log)
-
-doc_id = ???
+// doc_id = ???
 
 
 aMedicalRecord = "A patient was just born and he is in a perfect health. Congrats!"
 
 
-// eosDoc.contract('medeos111111').then(medeos111111 => medeos111111.addrecord({"docid":doc_id,"data":"A good doctor"}).then(res => console.log("result", result))).catch(err=>console.log)
+patientsXpub = "xpub661MyMwAqRbcH2Z5RtM6ydu98YudxiUDTaBESx9VgXpURBCDdWGezitJ8ormADG6CsJPs23fLmaeLp8RJgNvFo6YJkGhpXnHusCkRhGZdqr"
+
+theNextDerivationId = 1;
+
+derivationPath = "m/44'/196'/0'/0/"+theNextDerivationId;
 
 
-
-
-// exit(0)
-
-const masterPrivateKey = eoscob.fromMasterSeed('patients_top_secret_master_seed');
-const mpk = masterPrivateKey.getPublicExtendedKey();
-const readonlyMasterPublicKey = eoscob.fromExtendedKey(mpk);
-const pubkey = masterPrivateKey.getPublicKey(); 
-console.log("pubkey:", pubkey);
-
-const derivationPath = "m/44'/196'/0'/0/0";
-
-const derivedPubKey = readonlyMasterPublicKey.deriveChild(derivationPath);
+masterPublicKey = eoscob.fromExtendedKey(patientsXpub);
+derivedPubKey = masterPublicKey.deriveChild(derivationPath);
 console.log("derivedPubKey:", derivedPubKey.getPublicKey());
-const derivedPrivKey = masterPrivateKey.deriveChild(derivationPath);
-console.log("pubkey of derivedPrivKey:", derivedPrivKey.getPublicKey());
-// console.log("pub:",derivedPubKey);
-// console.log("priv:",derivedPrivKey);
 
-data = "ateasfasd fst data"
-console.log("original data:", data);
+
 
 
 doctorsPrivateKey=ecc.PrivateKey.fromSeed('someone')
 doctorsPublicKey=doctorsPrivateKey.toPublic()
+encryptedMessage = ecc.Aes.encrypt(doctorsPrivateKey, derivedPubKey.getPublicKey(), aMedicalRecord)
+simple = JSON.stringify({ nonce: encryptedMessage.nonce, message: encryptedMessage.message.toString("base64"), checksum: encryptedMessage.checksum })
+console.log("encryptedMessage:",simple)
 
-encryptedMessage = ecc.Aes.encrypt(doctorsPrivateKey, derivedPubKey.getPublicKey(), data)
-console.log("encryptedMessage: ", encryptedMessage)
-message = ecc.Aes.decrypt(derivedPrivKey.getPrivateKey(), doctorsPublicKey,
-  encryptedMessage.nonce, encryptedMessage.message, encryptedMessage.checksum);
 
-console.log("result message:", message.toString());
+
+// message = ecc.Aes.decrypt(derivedPrivKey.getPrivateKey(), doctorsPublicKey,
+//   encryptedMessage.nonce, encryptedMessage.message, encryptedMessage.checksum);
+
+// console.log("result message:", message.toString());
+
+ 
+record = {"doc":"medeosdocto1","rid":derivedPubKey.getPublicKey(),"data": simple }
+
+
+
+// eosDoc.contract('medeos111111').then(medeos111111 => medeos111111.addrecord(record).then(res => console.log("result", result))).catch(err=>console.log)
+
+
+
+
+//get record 
+
+// curl https://api-kylin.eosasia.one/v1/chain/get_table_rows \
+//   -X POST \
+//   -d '{ "json" : true, "code":"medeos111111", "scope":"medeos111111", "table":"record", "key_type":"sha256", "index_position":2, "limit":1, "upper_bound":"f459061974c976fe554dcbdb50617ee0cacaccdd7c3310d98cc0afbf9da904f0"}'
+
+
+rawKey = derivedPubKey.getPublicKey();
+// console.log("original raw key:", rawKey)
+// console.log("truncated raw key:", rawKey.substr(3))
+
+// var decoded = Base58.decode(rawKey.substr(3));
+// console.log("decoded:", decoded)
+// console.log("decoded.elngth:", decoded.length)
+// decoded = decoded.slice(1, decoded.length - 4)
+// console.log("decoded2.elngth:", decoded.length)
+// console.log("decoded2.hex", Buffer.from(decoded).toString("hex"))
+rawKey = ecc.PublicKey(rawKey).toHex();
+rawKey = rawKey.substr(2);
+console.log("raw key:", rawKey)
+console.log("raw key len:", rawKey.length)
+
+
+console.log("needLength:", "f459061974c976fe554dcbdb50617ee0cacaccdd7c3310d98cc0afbf9da904f0".length)
+
+// rawKey = rawKey[3:]lower_bound: id, upper_bound: id+1
+eosDoc.getTableRows({json:true, scope: "medeos111111", code: "medeos111111", table: "record", limit:1, key_type:"sha256", index_position:2, upper_bound: rawKey}).then(res => { console.log("res:",res);let row = res.rows[0]; console.log("row:",row)}).catch(err=>console.log);
+
+// // exit(0)
+
+// const masterPrivateKey = eoscob.fromMasterSeed('patients_top_secret_master_seed');
+// const mpk = masterPrivateKey.getPublicExtendedKey(); //xpub661MyMwAqRbcH2Z5RtM6ydu98YudxiUDTaBESx9VgXpURBCDdWGezitJ8ormADG6CsJPs23fLmaeLp8RJgNvFo6YJkGhpXnHusCkRhGZdqr
+// console.log("mpk:",mpk)
+// const readonlyMasterPublicKey = eoscob.fromExtendedKey(mpk);
+// const pubkey = masterPrivateKey.getPublicKey(); 
+// console.log("pubkey:", pubkey);
+
+// const derivationPath = "m/44'/196'/0'/0/0";
+
+// const derivedPubKey = readonlyMasterPublicKey.deriveChild(derivationPath);
+// console.log("derivedPubKey:", derivedPubKey.getPublicKey());
+// const derivedPrivKey = masterPrivateKey.deriveChild(derivationPath);
+// console.log("pubkey of derivedPrivKey:", derivedPrivKey.getPublicKey());
+// // console.log("pub:",derivedPubKey);
+// // console.log("priv:",derivedPrivKey);
+
+// data = "ateasfasd fst data"
+// console.log("original data:", data);
+
+
+// doctorsPrivateKey=ecc.PrivateKey.fromSeed('someone')
+// doctorsPublicKey=doctorsPrivateKey.toPublic()
+
+// encryptedMessage = ecc.Aes.encrypt(doctorsPrivateKey, derivedPubKey.getPublicKey(), data)
+// console.log("encryptedMessage: ", encryptedMessage)
+// message = ecc.Aes.decrypt(derivedPrivKey.getPrivateKey(), doctorsPublicKey,
+//   encryptedMessage.nonce, encryptedMessage.message, encryptedMessage.checksum);
+
+// console.log("result message:", message.toString());
 
